2313ef9b4250838798ad2681fc34351ba3a9843f
Xing Qingyu <201330010330@hnu.edu.cn>
2021-01-31 17:50:49 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# Config Caddy
+cat << EOF > /etc/caddy/Caddyfile
+:$PORT
+root * /usr/share/caddy
+file_server
+@websockets_heroku {
+header Connection *Upgrade*
+header Upgrade    websocket
+path $WSPATH
+}
+reverse_proxy @websockets_heroku 127.0.0.1:14514
+EOF
+
 # Download and install Xray
 mkdir /tmp/xray
 curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
@@ -13,32 +26,35 @@ rm -rf /tmp/xray
 install -d /usr/local/etc/xray
 cat << EOF > /usr/local/etc/xray/config.json
 {
-    "inbounds": [
+    "inbounds": 
+    [
         {
-            "port": $PORT,
-            "protocol": "vless",
-            "settings": {
-                "clients": [
-                    {
-                        "id": "$UUID",
-                        "level": 0
-                    }
-                ],
-                 "decryption": "none",
-                "disableInsecureEncryption": true
-            },
-            "streamSettings": {
-                "network": "ws"
-            }
+            "port": 14514,"listen": "127.0.0.1","protocol": "vless",
+            "settings": {"clients": [{"id": "$UUID"}],"decryption": "none"},
+            "streamSettings": {"network": "ws","wsSettings": {"path": "/"}}
         }
     ],
-    "outbounds": [
-        {
-            "protocol": "freedom"
-        }
-    ]
-}
+	
+    "outbounds": 
+    [
+        {"protocol": "freedom","tag": "direct","settings": {}},
+        {"protocol": "socks","tag": "sockstor","settings": {"servers": [{"address": "127.0.0.1","port": 9050}]}},
+        {"protocol": "blackhole","tag": "blocked","settings": {}}
+    ],
+	
+    "routing": 
+    {
+        "rules": 
+        [
+            {"type": "field","outboundTag": "blocked","ip": ["geoip:private","geoip:cn"]},
+            {"type": "field","outboundTag": "blocked","domain": ["geosite:private","geosite:cn","geosite:category-ads-all"]},
+            {"type": "field","outboundTag": "sockstor","domain": ["geosite:tor"]}
+        ]
+    }
+}	
 EOF
 
+nohup tor &
+caddy run --config /etc/caddy/Caddyfile --adapter caddyfile &
 # Run V2Ray
 /usr/local/bin/xray -config /usr/local/etc/xray/config.json
